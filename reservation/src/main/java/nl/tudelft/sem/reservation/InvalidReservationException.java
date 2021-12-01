7f375d8eb8e9c1d0b11207772b4e8ecc2c8c5e2f
Cherin Kim <C.Kim-1@student.tudelft.nl>
2021-12-01 16:15:19 +0100

    Fix PMD errors on reservation

--- reservation/src/main/java/nl/tudelft/sem/reservation/InvalidReservationException.java
+++ reservation/src/main/java/nl/tudelft/sem/reservation/InvalidReservationException.java
@@ -2,6 +2,8 @@ package nl.tudelft.sem.reservation;
 
 public class InvalidReservationException extends Exception {
 
+    public static final long serialVersionUID = 1L;
+
     public InvalidReservationException(String errorMessage) {
         super(errorMessage);
     }
--- room/src/main/java/nl/tudelft/sem/room/communication/ServerCommunication.java
+++ room/src/main/java/nl/tudelft/sem/room/communication/ServerCommunication.java
@@ -12,17 +12,17 @@ public class ServerCommunication {
     protected static Gson gson = new Gson();
 
     protected static HttpResponse<String> requestHandler(HttpRequest request) {
-        HttpResponse<String> response;
         try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusOK = 200;
+            if (response.statusCode() != statusOK) {
+                System.out.println("Status: " + response.statusCode());
+                return null;
+            }
+            return response;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-        if (response.statusCode() != 200) {
-            System.out.println("Status: " + response.statusCode());
-            return null;
-        }
-        return response;
     }
 }

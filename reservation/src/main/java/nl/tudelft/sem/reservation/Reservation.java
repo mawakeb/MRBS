3046ef390b53627ff204f8098e82a2ab2cd43f4a
Cherin Kim <C.Kim-1@student.tudelft.nl>
2021-12-01 15:31:34 +0100

    Make some methods in Reservation

--- reservation/src/main/java/nl/tudelft/sem/reservation/Reservation.java
+++ reservation/src/main/java/nl/tudelft/sem/reservation/Reservation.java
@@ -1,36 +1,52 @@
 package nl.tudelft.sem.reservation;
 
-import java.time.LocalTime;
-import java.time.ZonedDateTime;
-import java.util.UUID;
+import java.time.LocalDateTime;
 
 public class Reservation {
 
     private int id;
     private int roomID;
     private String userID;
-    private LocalTime start;
-    private LocalTime end;
+    private LocalDateTime start;
+    private LocalDateTime end;
     private String purpose;
     private String editPurpose;
+    private boolean cancelled;
 
 
     /**
      * Constructor for the Reservation class.
      *
-     * @param id      unique room UUID.
-     * @param roomID  title of the room chosen by lecturer.
-     * @param userID  room access code for the staff role.
-     * @param start   room access code for the student role.
-     * @param end     room access code for the student role.
-     * @param purpose room access code for the student role.
+     * @param id      unique reservation ID.
+     * @param roomID  location of the reservation.
+     * @param userID  netID of the user who made the reservation.
+     * @param start   Date and time of the start of the reservation
+     * @param end     Date and time of the end of the reservation
+     * @param purpose purpose of the reservation
      */
-    public Reservation(int id, int roomID, String userID, LocalTime start, LocalTime end, String purpose) {
+    public Reservation(int id, int roomID, String userID, LocalDateTime start, LocalDateTime end, String purpose) {
         this.id = id;
         this.roomID = roomID;
         this.userID = userID;
         this.start = start;
         this.end = end;
         this.purpose = purpose;
+        this.cancelled = false;
+    }
+
+    public void changeLocation(int newRoomID, String editPurpose){
+        this.roomID = newRoomID;
+        this.editPurpose = editPurpose;
+    }
+
+    public void changeTime(LocalDateTime newStart, LocalDateTime newEnd, String editPurpose){
+        this.start = newStart;
+        this.end = newEnd;
+        this.editPurpose = editPurpose;
+    }
+
+    public void cancelReservation(int newRoomID, String editPurpose){
+        this.cancelled = true;
+        this.editPurpose = editPurpose;
     }
 }

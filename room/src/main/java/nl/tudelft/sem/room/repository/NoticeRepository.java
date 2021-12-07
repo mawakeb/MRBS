d6e1bc48a5d5253e952ecb2bb01fe26a570e2084
Cherin Kim <c.kim-1@student.tudelft.nl>
2021-12-07 20:50:03 +0100

    Add RoomNoticeService
    to allow user to leave a notice regarding the room's maintenance

---
+++ room/src/main/java/nl/tudelft/sem/room/repository/NoticeRepository.java
@@ -0,0 +1,23 @@
+package nl.tudelft.sem.room.repository;
+
+import nl.tudelft.sem.room.entity.RoomNotice;
+import org.h2.command.dml.Select;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+
+/**
+ * The RoomNotice Repository Interface.
+ */
+public interface NoticeRepository extends JpaRepository<RoomNotice, Long> {
+
+    List<RoomNotice> findAll();
+
+    RoomNotice findById(long id);
+
+
+
+}
--- room/src/main/java/nl/tudelft/sem/room/service/RoomServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package nl.tudelft.sem.room.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class RoomServiceImpl implements RoomService {
-}

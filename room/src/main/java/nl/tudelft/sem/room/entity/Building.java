2789fe6d9b49b6b3e84cb5ebbb460ec557f581ca
Cherin Kim <c.kim-1@student.tudelft.nl>
2021-12-07 15:19:13 +0100

    Added entities required for the service

---
+++ room/src/main/java/nl/tudelft/sem/room/entity/Building.java
@@ -0,0 +1,83 @@
+package nl.tudelft.sem.room.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalTime;
+import java.util.Objects;
+
+/**
+ * The Building Entity.
+ */
+@Entity
+@Table(name = "building")
+public class Building {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "openTime")
+    private LocalTime openTime;
+
+    @Column(name = "closeTime")
+    private LocalTime closeTime;
+
+    public Building() {}
+
+    public Building(Long id, String name, LocalTime openTime, LocalTime closeTime) {
+        this.id = id;
+        this.name = name;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalTime getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(LocalTime openTime) {
+        this.openTime = openTime;
+    }
+
+    public LocalTime getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(LocalTime closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Building building = (Building) o;
+        return Objects.equals(id, building.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

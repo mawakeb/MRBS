c4d28a1bcf68739160de02a50ce54d548d019550
Cherin Kim <c.kim-1@student.tudelft.nl>
2021-12-07 22:04:21 +0100

    Add mapping to checkUser in ReservationController
    to check if the given user is the user who made the reservation

--- reservation/src/main/java/nl/tudelft/sem/reservation/communication/ReservationController.java
+++ reservation/src/main/java/nl/tudelft/sem/reservation/communication/ReservationController.java
@@ -1,22 +1,34 @@
 package nl.tudelft.sem.reservation.communication;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import nl.tudelft.sem.reservation.entity.Reservation;
+import nl.tudelft.sem.reservation.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("reservation")
 public class ReservationController {
 
-    /*
+
+    private final transient ReservationRepository reservationRepo;
+    protected static Gson gson = new Gson();
+
     @Autowired
-    LectureRepository lectureRepository;
-    @Autowired
-    UserRepository userRepository;*/
+    public ReservationController(ReservationRepository reservationRepo) {
+        this.reservationRepo = reservationRepo;
+    }
+
 
     /**
      * Gets all lectures.
@@ -27,6 +39,21 @@ public class ReservationController {
     public String returnHi() {
         //return lectureRepository.findAll();
         return "hello_from_reservation";
+    }
+
+    @GetMapping("checkUser")
+    public boolean checkUser(@RequestBody String q) {
+        List<String> list = gson.fromJson(q, new TypeToken<List<String>>() {}.getType());
+        long userId = Long.parseLong(list.get(0));
+        long reservationId = Long.parseLong(list.get(1));
+        Reservation reservation = reservationRepo.findById(reservationId).orElse(null);
+        if (reservation!=null){
+            if (reservation.getUserId().equals(userId)) return true;
+            else return false;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "RESERVATION_NOT_FOUND");
+        }
+        return false;
     }
 
     /*@GetMapping("getByJoinCode")

b74794b3fb9fe36b9e4ed3f5973083c3297f53ea
Pau Faraldos <P.FaraldosPijoan@student.tudelft.nl>
2021-12-09 15:29:27 +0100

    Created eureka and zuul applications as well as connected existing microservices to these

--- reservation/src/main/java/nl/tudelft/sem/reservation/communication/ReservationController.java
+++ reservation/src/main/java/nl/tudelft/sem/reservation/communication/ReservationController.java
@@ -7,17 +7,15 @@ import nl.tudelft.sem.reservation.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("reservation")
+@RequestMapping("")
 public class ReservationController {
 
 

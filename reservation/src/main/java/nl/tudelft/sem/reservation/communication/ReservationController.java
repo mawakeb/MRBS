2fcd0d49f7442fe5cf3623d64f4954569bb68198
Thijs Penning <t.j.penning@student.tudelft.nl>
2021-12-10 13:34:21 +0100

    Fix build

--- reservation/src/main/java/nl/tudelft/sem/reservation/communication/ReservationController.java
+++ reservation/src/main/java/nl/tudelft/sem/reservation/communication/ReservationController.java
@@ -5,10 +5,7 @@ import nl.tudelft.sem.reservation.entity.Reservation;
 import nl.tudelft.sem.reservation.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;

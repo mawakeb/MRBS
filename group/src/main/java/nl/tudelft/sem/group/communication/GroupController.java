7541fa4cb6af25fae2fcd57bf2d31c567d706612
[Timo <t.heijnemans@student.tudelft.nl>
2022-01-07 23:15:53 +0100

    Implement methods still needed for the validators (unfinished)

--- group/src/main/java/nl/tudelft/sem/group/communication/GroupController.java
+++ group/src/main/java/nl/tudelft/sem/group/communication/GroupController.java
@@ -31,6 +31,52 @@ public class GroupController {
         }
     }
 
+    @GetMapping("isSecretaryOfGroup")
+    public boolean isSecretaryOfGroup(Long secretaryId, Long groupId)
+    {
+        List<Long> secretries = getSecretariesId(groupId);
+        return secretries.contains(secretaryId);
+    }
+
+    @GetMapping("isSecretaryOfUser")
+    public boolean isSecretaryOfUser(Long secretaryId, Long employeeId)
+    {
+        return false;
+    }
+
+    @GetMapping("isInGroup")
+    public boolean isInGroup(Long userId, Long groupId)
+    {
+        Group group = groupRepository.findById(groupId).orElse(null);
+
+        if (group == null) {
+            throw new GroupNotFoundException("Group was not found.");
+        } else {
+            return group.getMembersIds().contains(userId);
+        }
+    }
+
+    //returns true iff there is at least one employee in group 1 that is also in group 2
+    @GetMapping("overlap")
+    public boolean overlap(Long groupId1, Long groupId2)
+    {
+        Group group1 = groupRepository.findById(groupId1).orElse(null);
+        Group group2 = groupRepository.findById(groupId2).orElse(null);
+
+        if (group1 == null || group2 == null) {
+            throw new GroupNotFoundException("Groups were not found.");
+        }
+
+        List<Long> members1 = group.getMembersIds();
+        List<Long> members2 = group.getMembersIds();
+
+        for(member : members1)
+        {
+            if(members2.contains(member)) return true;
+        }
+        return false;
+    }
+
     @PostMapping("createGroup")
     public String createGroup(@RequestParam List<Long> secretaryIds, @RequestParam List<Long> membersIds) {
         if (UserCommunication.getCurrentUserType().equals("ADMIN")) {

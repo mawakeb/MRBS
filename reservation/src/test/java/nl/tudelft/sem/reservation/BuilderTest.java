9e8c56ace34fe0e5740f3eef8a613a87f5a12d79
[Timo <t.heijnemans@student.tudelft.nl>
2022-01-08 11:49:21 +0100

    Add simple builder test

---
+++ reservation/src/test/java/nl/tudelft/sem/reservation/BuilderTest.java
@@ -0,0 +1,72 @@
+package nl.tudelft.sem.reservation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import nl.tudelft.sem.reservation.builder.Director;
+import nl.tudelft.sem.reservation.builder.Builder;
+import nl.tudelft.sem.reservation.builder.ReservationBuilder;
+import nl.tudelft.sem.reservation.entity.Reservation;
+import nl.tudelft.sem.reservation.entity.ReservationType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+
+public class BuilderTest {
+
+    Long madeBy = 123L;
+    Long roomId = 321L;
+    LocalDateTime start = LocalDateTime.of(1,2,3,4,5);
+    LocalDateTime end = LocalDateTime.of(5,4,3,2,1);
+    Builder testBuilder;
+    Director testDirector;
+
+    @BeforeEach
+    public void setValues()
+    {
+        testBuilder = new ReservationBuilder(madeBy, roomId, start, end);
+        testDirector = new Director(testBuilder);
+    }
+
+    @Test
+    public void selfReservationTest() {
+
+        testDirector.buildSelfReservation();
+        Reservation testReservation = testBuilder.build();
+
+        assertEquals(madeBy, testReservation.getMadeBy());
+        assertEquals(roomId, testReservation.getRoomId());
+        assertEquals(start, testReservation.getStart());
+        assertEquals(end, testReservation.getEnd());
+        assertEquals(madeBy, testReservation.getUserId());
+        assertEquals(ReservationType.SELF, testReservation.getType());
+    }
+
+    @Test
+    public void adminReservationTest() {
+
+        testDirector.buildAdminReservation(234L);
+        Reservation testReservation = testBuilder.build();
+
+        assertEquals(madeBy, testReservation.getMadeBy());
+        assertEquals(roomId, testReservation.getRoomId());
+        assertEquals(start, testReservation.getStart());
+        assertEquals(end, testReservation.getEnd());
+        assertEquals(234L, testReservation.getUserId());
+        assertEquals(ReservationType.ADMIN, testReservation.getType());
+    }
+
+    @Test
+    public void groupReservationTest() {
+
+        testDirector.buildGroupReservation(345L, "Test purpose");
+        Reservation testReservation = testBuilder.build();
+
+        assertEquals(madeBy, testReservation.getMadeBy());
+        assertEquals(roomId, testReservation.getRoomId());
+        assertEquals(start, testReservation.getStart());
+        assertEquals(end, testReservation.getEnd());
+        assertEquals(345L, testReservation.getGroupId());
+        assertEquals("Test purpose", testReservation.getPurpose());
+        assertEquals(ReservationType.GROUP, testReservation.getType());
+    }
+}

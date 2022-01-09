a7e8ffb376574ecceee34aa0e847a05ee2c4ee7f
Thijs Penning <t.j.penning@student.tudelft.nl>
2022-01-09 23:23:06 +0100

    Fix checkstyle

--- group/src/main/java/nl/tudelft/sem/group/entity/Group.java
+++ group/src/main/java/nl/tudelft/sem/group/entity/Group.java
@@ -2,14 +2,12 @@ package nl.tudelft.sem.group.entity;
 
 import java.util.List;
 import java.util.Objects;
-import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 /**

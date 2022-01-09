bd7359b469d90727231ccfe9cc7c81e0fc9c0757
[Timo <t.heijnemans@student.tudelft.nl>
2022-01-09 22:46:11 +0100

    Add javadoc to the Reservation class

--- reservation/src/main/java/nl/tudelft/sem/reservation/entity/Reservation.java
+++ reservation/src/main/java/nl/tudelft/sem/reservation/entity/Reservation.java
@@ -72,105 +72,194 @@ public class Reservation {
         this.cancelled = false;
     }
 
+    /**
+     * @return  The ID of this reservation
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * @param id    The new ID of this reservation
+     */
     private void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * @return  A Long representing the room this reservation is in
+     */
     public Long getRoomId() {
         return roomId;
     }
 
+    /**
+     * @param roomId    A Long representing the new room this reservation should be in
+     */
     private void setRoomId(Long roomId) {
         this.roomId = roomId;
     }
 
+    /**
+     * @return  A Long representing the user who made this reservation
+     */
     public Long getMadeBy() {
         return madeBy;
     }
 
+    /**
+     * @param madeBy    A Long representing the user who made this reservation
+     */
     private void setMadeBy(Long madeBy) {
         this.madeBy = madeBy;
     }
 
+
+    /**
+     * @return  A LocalDateTime representing the time this reservation starts or has started
+     */
     public LocalDateTime getStart() {
         return start;
     }
 
+    /**
+     * @param start A LocalDateTime representing the time this reservation should start
+     */
     private void setStart(LocalDateTime start) {
         this.start = start;
     }
 
+    /**
+     * @return  A LocalDateTime representing the time this reservation ends or has ended
+     */
     public LocalDateTime getEnd() {
         return end;
     }
 
+    /**
+     * @param end   A LocalDateTime representing the time this reservation should end
+     */
     private void setEnd(LocalDateTime end) {
         this.end = end;
     }
 
+
+    /**
+     * @return  A String describing why this reservation was made
+     */
     public String getPurpose() {
         return purpose;
     }
 
+    /**
+     * @param purpose   A String describing why this reservation was made
+     */
     private void setPurpose(String purpose) {
         this.purpose = purpose;
     }
 
+    /**
+     * @return  A String describing why this reservation was changed
+     */
     public String getEditPurpose() {
         return editPurpose;
     }
 
+    /**
+     * @param editPurpose   A String describing why this reservation was changed
+     */
     public void setEditPurpose(String editPurpose) {
         this.editPurpose = editPurpose;
     }
 
+    /**
+     * @return  A ReservationType representing what kind of reservation this is
+     */
     public ReservationType getType() {
         return type;
     }
 
+    /**
+     * @param type  A ReservationType representing what kind of reservation this should be
+     */
     private void setType(ReservationType type) {
         this.type = type;
     }
 
+
+    /**
+     * @return  A Long representing the user this reservation was made for
+     */
     public long getUserId() {
         return userId;
     }
 
-    private long setUserId() {
-        return userId;
+    /**
+     * @  A Long representing the user this reservation should have been made for
+     */
+    private void setUserId(long UserId) {
+        this.userId = userId;
     }
 
+    /**
+     * @  A Long representing the group this reservation was made for
+     */
     public long getGroupId() {
         return groupId;
     }
 
-    private long setGroupId() {
-        return groupId;
+    /**
+     * @  A Long representing the group this reservation should have been made for
+     */
+    private void setGroupId(long groupId) {
+        this.groupId = groupId;
     }
 
+
+    /**
+     * @return  A boolean representing if this reservation has been cancelled
+     */
     public boolean isCancelled() {
         return cancelled;
     }
 
+    /**
+     * @return  A boolean representing if this reservation should be cancelled
+     */
     private void setCancelled(boolean cancelled) {
         this.cancelled = cancelled;
     }
 
+
+    /**
+     * Changes the location of the reservation
+     *
+     * @param newRoomId     A Long representing the new room the reservation should be set in
+     * @param editPurpose   A String explaining why this reservation was moved
+     */
     public void changeLocation(Long newRoomId, String editPurpose) {
         this.setRoomId(newRoomId);
         this.setEditPurpose(editPurpose);
     }
 
+    /**
+     * Changes the date and/or time of the reservation
+     *
+     * @param newStart      A LocalDateTime representing when the reservation should start
+     * @param newEnd        A LocalDateTime representing when the reservation should end
+     * @param editPurpose   A String explaining why this reservation was rescheduled
+     */
     public void changeTime(LocalDateTime newStart, LocalDateTime newEnd, String editPurpose) {
         this.setStart(newStart);
         this.setEnd(newEnd);
         this.setEditPurpose(editPurpose);
     }
 
+    /**
+     * Cancels the reservation
+     *
+     * @param editPurpose   A String explaining why this reservation was cancelled
+     */
     public void cancelReservation(String editPurpose) {
         this.setCancelled(true);
         this.setEditPurpose(editPurpose);

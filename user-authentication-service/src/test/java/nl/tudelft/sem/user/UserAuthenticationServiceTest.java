95d7ea7a6a1cbe3ed492d91b7673af3059a2f00c
Pau Faraldos <P.FaraldosPijoan@student.tudelft.nl>
2022-01-09 18:02:15 +0100

    Refactored methods and classes

--- user-authentication-service/src/test/java/nl/tudelft/sem/user/UserAuthenticationServiceTest.java
+++ user-authentication-service/src/test/java/nl/tudelft/sem/user/UserAuthenticationServiceTest.java
@@ -1,10 +1,8 @@
 package nl.tudelft.sem.user;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
-public class ExampleTest {
+public class UserAuthenticationServiceTest {
 
     @Test
     public void testGettersSetters() {

816df3a3a05418c7886ad920253c35f2617761fb
Thijs Penning <t.j.penning@student.tudelft.nl>
2022-01-09 23:19:02 +0100

    Fix group not running

--- group/src/main/java/nl/tudelft/sem/group/entity/Group.java
+++ group/src/main/java/nl/tudelft/sem/group/entity/Group.java
@@ -2,11 +2,14 @@ package nl.tudelft.sem.group.entity;
 
 import java.util.List;
 import java.util.Objects;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 /**
@@ -21,9 +24,11 @@ public class Group {
     @Column(name = "id")
     private Long id;
 
+    @ElementCollection
     @Column(name = "secretaries")
     private List<Long> secretaryIds;
 
+    @ElementCollection
     @Column(name = "members")
     private List<Long> membersIds;
 

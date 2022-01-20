83ca06e086f791370f43942b8ecbe271012a9404
Thijs Penning <t.j.penning@student.tudelft.nl>
2022-01-20 16:32:20 +0100

    Add tests for the reservation entity to improve the mutation score

--- reservation-service/src/test/java/nl/tudelft/sem/reservation/entity/ReservationTest.java
+++ reservation-service/src/test/java/nl/tudelft/sem/reservation/entity/ReservationTest.java
@@ -20,7 +20,6 @@ public class ReservationTest {
 
     @BeforeEach
     void setUp() {
-
         Builder testBuilder = new ReservationBuilder(123L, 321L, start, end);
         Director testDirector = new Director(testBuilder);
         testDirector.buildSingleReservation(234L, 345L, "Test purpose");
@@ -90,5 +89,127 @@ public class ReservationTest {
         assertTrue(reservation.isCancelled());
         assertEquals("Cancel purpose", reservation.getEditPurpose());
 
+    }
+
+    // equals method tests
+    @Test
+    public void equalTest() {
+        Builder testBuilder = new ReservationBuilder(123L, 321L, start, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 345L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        assertTrue(reservation.equals(other));
+    }
+
+    @Test
+    public void differentCancelled() {
+        Builder testBuilder = new ReservationBuilder(123L, 321L, start, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 345L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        other.cancelReservation(null);
+
+        assertFalse(reservation.equals(other));
+    }
+
+    @Test
+    public void differentMadeBy() {
+        Builder testBuilder = new ReservationBuilder(456L, 321L, start, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 345L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        assertFalse(reservation.equals(other));
+    }
+
+    @Test
+    public void differentRoomId() {
+        Builder testBuilder = new ReservationBuilder(123L, 654L, start, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 345L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        assertFalse(reservation.equals(other));
+    }
+
+    @Test
+    public void differentStart() {
+        LocalDateTime otherStart = LocalDateTime.of(1, 2, 3, 4, 6);
+        Builder testBuilder = new ReservationBuilder(123L, 321L, otherStart, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 345L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        assertFalse(reservation.equals(other));
+    }
+
+    @Test
+    public void differentEnd() {
+        LocalDateTime otherEnd = LocalDateTime.of(5, 4, 3, 2, 0);
+        Builder testBuilder = new ReservationBuilder(123L, 321L, start, otherEnd);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 345L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        assertFalse(reservation.equals(other));
+    }
+
+    @Test
+    public void differentType() {
+        Builder testBuilder1 = new ReservationBuilder(123L, 321L, start, end);
+        Director testDirector1 = new Director(testBuilder1);
+        testDirector1.buildSelfReservation();
+        Reservation one = testBuilder1.build();
+
+        Builder testBuilder2 = new ReservationBuilder(123L, 321L, start, end);
+        Director testDirector2 = new Director(testBuilder2);
+        testDirector2.buildAdminReservation(123L);
+        Reservation other = testBuilder2.build();
+
+        assertFalse(one.equals(other));
+    }
+
+    @Test
+    public void differentUserId() {
+        Builder testBuilder = new ReservationBuilder(123L, 321L, start, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(567L, 345L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        assertFalse(reservation.equals(other));
+    }
+
+    @Test
+    public void differentGroupId() {
+        Builder testBuilder = new ReservationBuilder(123L, 321L, start, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 678L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        assertFalse(reservation.equals(other));
+    }
+
+    @Test
+    public void differentPurpose() {
+        Builder testBuilder = new ReservationBuilder(123L, 321L, start, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 345L, "Totally not testing purpose");
+        Reservation other = testBuilder.build();
+
+        assertFalse(reservation.equals(other));
+    }
+
+    @Test
+    public void differentEditPurpose() {
+        Builder testBuilder = new ReservationBuilder(123L, 321L, start, end);
+        Director testDirector = new Director(testBuilder);
+        testDirector.buildSingleReservation(234L, 345L, "Test purpose");
+        Reservation other = testBuilder.build();
+
+        other.changeLocation(321L, "Testing purposes");
+
+        assertFalse(reservation.equals(other));
     }
 }
